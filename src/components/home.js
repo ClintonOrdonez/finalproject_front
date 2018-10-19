9d7bfc43f4db521010b57cd2de57a81982d20b51
Ramy <ramyghabros@gmail.com>
2018-10-19 10:18:30 -0700

    ghjgjh

--- src/components/home.js
+++ src/components/home.js
@@ -4,7 +4,7 @@ const Home = props => (
   <div>
     <h3 className="cover-heading">Home</h3>
     <p className="lead" >
-      This is the landing page for our project. We will create an introduction
+      This is the landing page for our    project. We will create an introduction
       here.
     </p>
   </div>
