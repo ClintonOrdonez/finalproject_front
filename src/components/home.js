b839519015cda314d4e950af484c3aa7fdd7deb8
Clinton Dean Ordoñez <dasmexikaner@Clintons-iMac.local>
2018-10-19 10:37:40 -0700

    Changed home back, added CheckEmail action, added onBlur to Signup

--- src/components/home.js
+++ src/components/home.js
@@ -3,8 +3,8 @@ import React from "react";
 const Home = props => (
   <div>
     <h3 className="cover-heading">Home</h3>
-    <p className="lead" >
-      This is the landing page for our    project. We will create an introduction
+    <p className="lead">
+      This is the landing page for our project. We will create an introduction
       here.
     </p>
   </div>
--- src/components/signup.js
+++ src/components/signup.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
-import { UserSignup } from "../actions/actions";
+import { UserSignup, CheckEmail } from "../actions/actions";
 
 // This was to test whether the page would display and properly navigate
 // const Signup = props => (
@@ -12,18 +12,23 @@ import { UserSignup } from "../actions/actions";
 
 const mapDispatchToProps = dispatch => {
   return {
-    onSignup: (email, password) => dispatch(UserSignup(email, password))
+    onSignup: (email, password) => dispatch(UserSignup(email, password)),
+    onCheckEmail: email => dispatch(CheckEmail(email))
   };
 };
 
 const Signup = props => {
   let email;
+  let validEmail = false;
   let password;
+  let validPassword = false;
   let confirmPassword;
+  let passwordMatch = false;
 
   return (
     <div>
       <h3>Signup</h3>
+
       <div className="form-group">
         <input
           type="text"
@@ -32,8 +37,28 @@ const Signup = props => {
           ref={e => {
             email = e;
           }}
+          onBlur={() => {
+            props.onCheckEmail(email.value).then(result => {
+              if (result.data.count === 1) {
+                alert(
+                  "Email already in use; provide a different email or login."
+                );
+              } else {
+                validEmail = true;
+              }
+            });
+          }}
         />
+        <span />
       </div>
+
+      <div className="form-group">
+        <h6>
+          Passwords must be at least 8 characters, have 1 uppercase letter, 1
+          lowercase letter, and 1 number or 1 non-alphanumeric character.
+        </h6>
+      </div>
+
       <div className="form-group">
         <input
           type="password"
@@ -42,8 +67,10 @@ const Signup = props => {
           ref={p => {
             password = p;
           }}
+          onBlur={() => {}}
         />
       </div>
+
       <div className="form-group">
         <input
           type="password"
@@ -61,7 +88,7 @@ const Signup = props => {
           onClick={() => {
             if (password.value === confirmPassword.value) {
               props.onSignup(email.value, password.value);
-              alert("Signup successful; logging in now!");
+              alert("Signup successful; logging in now.");
               props.history.push("/");
             } else {
               alert("Passwords do not match.");
