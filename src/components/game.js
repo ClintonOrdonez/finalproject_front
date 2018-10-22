5629c77dcec837c438fc19dd1981bc24a2bb1d0d
Ramy <ramyghabros@gmail.com>
2018-10-22 10:14:16 -0700

    new page

--- src/components/game.js
+++ src/components/game.js
@@ -3,7 +3,7 @@ import React from "react";
 const Game = props => (
     <div>
         <h3 className="cover-heading">WELCOME!</h3>
-        <p>Start your game</p>
+        <p> Start your game </p>
 
 
     </div>
