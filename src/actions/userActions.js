d8e1273281297e5092cb0e6fd68ae53a3c2e83a8
Clinton Dean Ordoñez <dasmexikaner@Clintons-iMac.local>
2018-11-01 16:03:38 -0700

    Updated Heroku link, again

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -2,7 +2,7 @@ import { USER_LOGIN, USER_LOGOUT } from "./types";
 import axios from "axios";
 
 // let userURL = "http://localhost:8080/users";
-let userURL = "http://development-deployment-server.herokuapp.com/users";
+let userURL = "https://development-deployment-server.herokuapp.com/users";
 
 export const UserSignup = (email, password) => {
   return dispatch => {
