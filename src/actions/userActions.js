968696906c4946ad17b4b73ba0fd7764fc8a9e0b
Clinton Dean Ordoñez <dasmexikaner@Clintons-iMac.local>
2018-11-12 14:59:57 -0700

    Updated resetPassword.js alert

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,8 +1,8 @@
 import { USER_LOGIN, USER_LOGOUT } from "./types";
 import axios from "axios";
 
-let userURL = "http://localhost:8080/user";
-// let userURL = "https://development-deployment-server.herokuapp.com/user";
+// let userURL = "http://localhost:8080/user";
+let userURL = "https://development-deployment-server.herokuapp.com/user";
 
 export const UserSignup = (email, password) => {
   return dispatch => {
