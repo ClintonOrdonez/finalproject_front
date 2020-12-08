54f18d3664752cc18606720eab4b8960d94e1655
Clinton Dean Ordoñez <dasmexikaner@Clintons-iMac.local>
2020-12-08 14:56:00 -0700

    Added Discord link

--- src/components/home.js
+++ src/components/home.js
@@ -14,8 +14,9 @@ const Home = props => {
       <h3 className="cover-heading">Home</h3>
 
       <p className="lead">
-        This is the Team Gestalt project page. The database migration dragon has been slain and all may enjoy Tic-Tac-Toe once more. Did you know Sho-dan's first name is George?
+        This is the Team Gestalt project page. The database migration dragon has been slain and all may enjoy Tic-Tac-Toe once more.
       </p>
+      <a href="https://discord.gg/ynwVe9f2ET">Join the Team Gestalt Discord!</a>
       {props.token !== null && <h3>Welcome {props.email}!</h3>}
     </div>
   );
