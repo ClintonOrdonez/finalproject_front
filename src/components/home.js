032b6175c937d9dbb517c4e88b1d4dc23c36fd61
Clinton Dean Ordoñez <dasmexikaner@Clintons-iMac.local>
2020-12-08 14:42:16 -0700

    Updated Home page greeting

--- src/components/home.js
+++ src/components/home.js
@@ -14,7 +14,7 @@ const Home = props => {
       <h3 className="cover-heading">Home</h3>
 
       <p className="lead">
-        This is the Team Gestalt project page. The site is currently undergoing a database migration and will be opertaionl again soon.
+        This is the Team Gestalt project page. The database migration dragon has been slain and all may enjoy Tic-Tac-Toe once more. Did you know Sho-dan's first name is George?
       </p>
       {props.token !== null && <h3>Welcome {props.email}!</h3>}
     </div>
