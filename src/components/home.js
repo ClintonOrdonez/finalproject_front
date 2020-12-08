ccc32f3a77f9cf843a187c5ca24bbe6d758553a5
Clinton Dean Ordoñez <dasmexikaner@Clintons-iMac.local>
2020-12-08 13:30:31 -0700

    Changed comment lines to use deployment targets

--- src/components/home.js
+++ src/components/home.js
@@ -14,14 +14,7 @@ const Home = props => {
       <h3 className="cover-heading">Home</h3>
 
       <p className="lead">
-        This is the landing page for Team Gestalt's project. Our project
-        demonstrates a functional signup and login, all verified and tracked
-        through a database. Users have the ability to update their email and
-        password when logged in, reset their password if it has been forgotten,
-        and delete all their user information from the database. Furthermore,
-        users can select a theme of their liking which is saved and applied upon
-        login. Finally, users can play a riveting game of Tic-Tac-Toe with stats
-        that are dynamically tracked.
+        This is the Team Gestalt project page. The site is currently undergoing a database migration and will be opertaionl again soon.
       </p>
       {props.token !== null && <h3>Welcome {props.email}!</h3>}
     </div>
